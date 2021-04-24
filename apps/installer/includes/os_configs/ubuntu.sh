b395ca445d1ca9353fbd6255f1ebc2eb33c104b9
onixiya <29950414+Onixiya@users.noreply.github.com>
2021-04-24 21:18:59 +1000

    remove docker again

--- apps/installer/includes/os_configs/ubuntu.sh
+++ apps/installer/includes/os_configs/ubuntu.sh
@@ -9,14 +9,4 @@ sudo apt-get update -y
 
 # shared deps
 sudo apt-get -y install make cmake clang curl unzip libmysqlclient-dev libace-dev
-
-if [[ $CONTINUOUS_INTEGRATION || $DOCKER ]]; then
-  sudo apt-get -y install build-essential libtool cmake-data openssl libgoogle-perftools-dev \
-  libssl-dev libmysql++-dev libreadline6-dev zlib1g-dev libbz2-dev mysql-client \
-  libncurses5-dev ccache curl unzip
-else
-  sudo apt-get install -y git gcc g++ \
-  libssl-dev libbz2-dev libreadline-dev libncurses-dev \
-  mysql-server libace-6.*
-fi
-
+sudo apt-get install -y git gcc g++ libssl-dev libbz2-dev libreadline-dev libncurses-dev mysql-server libace-6.*
